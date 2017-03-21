985824d8858345dedf6d2b8660549e87c2728c7f
jairoeli <jairoelid@gmail.com>
2017-03-21 11:02:26 -0500

    Initial commit

---
+++ InstagramFirebase/InstagramFirebase/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  InstagramFirebase
+//
+//  Created by Jairo Eli de Leon on 3/21/17.
+//  Copyright © 2017 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+
+
+}
+
