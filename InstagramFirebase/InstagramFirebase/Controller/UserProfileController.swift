8ea91017fd54038b65342fd45a80b148bb90e084
jairoeli <jairoelid@gmail.com>
2017-03-22 19:08:09 -0500

    edit: fetchUser()
    fetch the user's username and display it at the top of our
    UINavigationBar.

--- InstagramFirebase/InstagramFirebase/Controller/UserProfileController.swift
+++ InstagramFirebase/InstagramFirebase/Controller/UserProfileController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class UserProfileController: UICollectionViewController {
 
@@ -14,7 +15,26 @@ class UserProfileController: UICollectionViewController {
     super.viewDidLoad()
     
     collectionView?.backgroundColor = .white
-    navigationItem.title = "User Profile"
+    navigationItem.title = FIRAuth.auth()?.currentUser?.uid
+    
+    fetchUser()
+  }
+  
+  fileprivate func fetchUser() {
+    guard let uid = FIRAuth.auth()?.currentUser?.uid else { return }
+    
+    FIRDatabase.database().reference().child("users").child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
+      print(snapshot.value ?? "")
+      
+      guard let dictionary = snapshot.value as? [String: Any] else { return }
+      
+      let username = dictionary["username"] as? String
+      self.navigationItem.title = username
+      
+    }) { (err) in
+      print("Failed to fetch user:", err)
+    }
+    
   }
   
 }
