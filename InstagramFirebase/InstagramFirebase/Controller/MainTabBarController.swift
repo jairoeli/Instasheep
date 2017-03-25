9b95832bbbf52c51a8a6c3eacfee8b79f17cfc9b
jairoeli <jairoelid@gmail.com>
2017-03-25 03:07:54 -0500

    add: logging in and logging out

--- InstagramFirebase/InstagramFirebase/Controller/MainTabBarController.swift
+++ InstagramFirebase/InstagramFirebase/Controller/MainTabBarController.swift
@@ -7,15 +7,25 @@
 //
 
 import UIKit
+import Firebase
 
 class MainTabBarController: UITabBarController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    if FIRAuth.auth()?.currentUser == nil {
+      // show if not logged in
+      DispatchQueue.main.async {
+        let loginController = LoginController()
+        let navController = UINavigationController(rootViewController: loginController)
+        self.present(navController, animated: true, completion: nil)
+      }
+      return
+    }
+    
     let layout = UICollectionViewFlowLayout()
     let userProfileController = UserProfileController(collectionViewLayout: layout)
-    
     
     let navController = UINavigationController(rootViewController: userProfileController)
     
