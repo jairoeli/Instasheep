be8145ddf0efaac1cad4f8c3b80024b2719bedeb
jairoeli <jairoelid@gmail.com>
2017-04-26 18:59:13 -0500

    Capture Photo

--- InstagramFirebase/InstagramFirebase/Camera/CameraController.swift
+++ InstagramFirebase/InstagramFirebase/Camera/CameraController.swift
@@ -9,7 +9,11 @@
 import UIKit
 import AVFoundation
 
-class CameraController: UIViewController {
+class CameraController: UIViewController, AVCapturePhotoCaptureDelegate {
+  
+  // MARK: - Properties
+  
+  let output = AVCapturePhotoOutput()
   
   lazy var dismissButton = UIButton(type: .system) <== {
     $0.setImage(#imageLiteral(resourceName: "right_arrow_shadow"), for: .normal)
@@ -21,6 +25,8 @@ class CameraController: UIViewController {
     $0.addTarget(self, action: #selector(handleCapturePhoto), for: .touchUpInside)
   }
   
+  // MARK: - View Life Cycle
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
@@ -28,14 +34,33 @@ class CameraController: UIViewController {
     setupHUD()
   }
   
-  // MARK: - Handle
+  func handleDismiss() {
+    dismiss(animated: true, completion: nil)
+  }
+  
+  // MARK: - Capture Photo
   
   func handleCapturePhoto() {
     print("Capturing photo...")
+    
+    let settings = AVCapturePhotoSettings()
+    guard let previewFormatType = settings.availablePreviewPhotoPixelFormatTypes.first else { return }
+    settings.previewPhotoFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewFormatType]
+    
+    output.capturePhoto(with: settings, delegate: self)
   }
   
-  func handleDismiss() {
-    dismiss(animated: true, completion: nil)
+  func capture(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?, previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+    
+    let imageData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: photoSampleBuffer!, previewPhotoSampleBuffer: previewPhotoSampleBuffer!)
+    
+    let previewImage = UIImage(data: imageData!)
+    
+    let previewImageView = UIImageView(image: previewImage)
+    view.addSubview(previewImageView)
+    previewImageView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+    
+    print("Finish processing photo sample buffer...")
   }
   
   // MARK: - setup
@@ -66,7 +91,6 @@ class CameraController: UIViewController {
     }
     
     // 2. setup outputs
-    let output = AVCapturePhotoOutput()
     if captureSession.canAddOutput(output) {
         captureSession.addOutput(output)
     }
