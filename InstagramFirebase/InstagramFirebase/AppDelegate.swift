97ee0092f30c59a0aab5754312a26ccbe811545e
jairoeli <jairoelid@gmail.com>
2017-12-09 18:54:52 -0600

    Setting up Push Notifications with FCM

--- InstagramFirebase/InstagramFirebase/AppDelegate.swift
+++ InstagramFirebase/InstagramFirebase/AppDelegate.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 import Firebase
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate {
 
   var window: UIWindow?
 
@@ -20,7 +21,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow()
     window?.rootViewController = MainTabBarController()
 
+    attemtpRegisterForNotifications(application: application)
+
     return true
+  }
+
+  func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    print("Registered for notifications:", deviceToken)
+  }
+
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+    print("Registered with FCM with token:", fcmToken)
+  }
+
+  // list for user notificaitons
+  func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    completionHandler(.alert)
+  }
+
+  private func attemtpRegisterForNotifications(application: UIApplication) {
+    print("Ateempting to register APNS...")
+
+    Messaging.messaging().delegate = self
+
+    UNUserNotificationCenter.current().delegate = self
+
+    // user notifications auth
+    // all of this works for iOS 10+
+    let options: UNAuthorizationOptions = [.alert, .badge, .sound]
+    UNUserNotificationCenter.current().requestAuthorization(options: options) { (granted, err) in
+      if let err = err {
+        print("Failed to request auth:", err)
+        return
+      }
+
+      if granted {
+        print("Auth granted.")
+      } else {
+        print("Auth denied.")
+      }
+    }
+
+    application.registerForRemoteNotifications()
   }
 
 }
